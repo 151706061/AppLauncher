c067d290b10668b71bcfe8b990ee5cd14c79f0ef
Julien Finet <julien.finet@kitware.com>
2011-11-15 05:49:00 -0500

    Compile the app launcher as a win32 application with no console
    Todo:
     - cleanup convertWindowsCommandLineToUnixArguments
     - force console outputs even with no console

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,17 @@
 #include <QTimer>
 #include <QDebug>
 
+#include "ctkAppLauncher.h"
+#include "ctkCommandLineParser.h"
+
 // STD includes
 #include <cstdlib>
 
-#include "ctkAppLauncher.h"
+#if defined (_WIN32)
+#include <windows.h>
+#endif
 
-int main(int argc, char** argv)
+int appLauncherMain(int argc, char** argv)
 {
   #ifdef QT_MAC_USE_COCOA
   // See http://doc.trolltech.com/4.7/qt.html#ApplicationAttribute-enum
@@ -27,3 +32,34 @@ int main(int argc, char** argv)
 
   return app.exec();
 }
+
+#if defined (_WIN32)
+int __stdcall WinMain(HINSTANCE hInstance,
+                      HINSTANCE hPrevInstance,
+                      LPSTR lpCmdLine, int nShowCmd)
+{
+  Q_UNUSED(hInstance);
+  Q_UNUSED(hPrevInstance);
+  Q_UNUSED(nShowCmd);
+
+  int argc;
+  char **argv;
+  ctkCommandLineParser::convertWindowsCommandLineToUnixArguments(
+    lpCmdLine, &argc, &argv);
+
+  int ret = appLauncherMain(argc, argv);
+
+  for (int i = 0; i < argc; i++)
+    {
+    delete [] argv[i];
+    }
+  delete [] argv;
+
+  return ret;
+}
+#else
+int main(int argc, char *argv[])
+{
+  return appLauncherMain(argc, argv);
+}
+#endif
